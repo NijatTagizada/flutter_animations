db8118ad2d1ae91ae95f9b0330e8c7f99c74d7a3
NijatTagizada <tagizada.nicat@gmail.com>
2022-03-06 22:11:40 +0400

    add animated button

---
+++ lib/home_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+import 'widgets/animated_button.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Homa Page',
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        child: Column(children: [
+          AnimatedButton(
+            onTap: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(content: Text('Pressed')),
+              );
+            },
+          )
+        ]),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/animated_button.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+class AnimatedButton extends StatefulWidget {
+  const AnimatedButton({Key? key, required this.onTap}) : super(key: key);
+  final Function onTap;
+
+  @override
+  _AnimatedButtonState createState() => _AnimatedButtonState();
+}
+
+class _AnimatedButtonState extends State<AnimatedButton>
+    with SingleTickerProviderStateMixin {
+  late double _scale;
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 200),
+      lowerBound: 0.0,
+      upperBound: 0.15,
+    )..addListener(() {
+        setState(() {});
+      });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _scale = 1 - _controller.value;
+    print(_controller.value);
+
+    return GestureDetector(
+      onTapDown: _onTapDown,
+      onTapUp: _onTapUp,
+      onTapCancel: _onTapCancel,
+      onTap: () {
+        widget.onTap();
+      },
+      child: Transform.scale(
+        scale: _scale,
+        child: _animatedButtonUI,
+      ),
+    );
+  }
+
+  void _onTapDown(TapDownDetails details) {
+    _controller.forward();
+  }
+
+  void _onTapUp(TapUpDetails details) {
+    _controller.reverse();
+  }
+
+  void _onTapCancel() {
+    _controller.reverse();
+  }
+
+  Widget get _animatedButtonUI => Container(
+        height: 50,
+        width: 200,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(100),
+          boxShadow: const [
+            BoxShadow(
+              color: Color(0x80000000),
+              blurRadius: 10.0,
+              offset: Offset(0.0, 4.0),
+            ),
+          ],
+          gradient: const LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Color.fromARGB(255, 141, 183, 255),
+              Color.fromARGB(255, 64, 131, 255),
+            ],
+          ),
+        ),
+        child: const Center(
+          child: Text(
+            'Press Me',
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      );
+}
